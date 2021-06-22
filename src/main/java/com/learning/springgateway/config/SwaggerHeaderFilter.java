ff728d31e5c389dc61b24cca56a1b579fd4093a6
qingru97 <qingru97@gmail.com>
2021-06-22 14:15:21 +0800

    Temporarily solution to get centralized docs to work - call APIs from swagger by adding another route predicate to gateway itself to do filtering

--- src/main/java/com/learning/springgateway/config/SwaggerHeaderFilter.java
+++ src/main/java/com/learning/springgateway/config/SwaggerHeaderFilter.java
@@ -3,32 +3,23 @@ package com.learning.springgateway.config;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
+// TODO: Fix swagger requests not coming into this filter when being called
 @Component
 public class SwaggerHeaderFilter extends AbstractGatewayFilterFactory {
     private static final String HEADER_NAME = "X-Forwarded-Prefix";
 
     @Override
     public GatewayFilter apply(Object config) {
-        System.out.println("apply");
         return (exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             String path = request.getURI().getPath();
-            System.out.println(path);
-
-            if (!StringUtils.endsWithIgnoreCase(path, "/v3/api-docs")) {
-                return chain.filter(exchange);
-            }
-
-            String basePath = path.substring(0, path.lastIndexOf("/v3/api-docs"));
-
-
-            ServerHttpRequest newRequest = request.mutate().header(HEADER_NAME, basePath).build();
-            System.out.println(newRequest.getPath());
+            String serviceName = request.getHeaders().get("Referer").get(0).split("primaryName=")[1];
+            String newPath = "/"+serviceName+path;
+            ServerHttpRequest newRequest = request.mutate().path(newPath).build();
             ServerWebExchange newExchange = exchange.mutate().request(newRequest).build();
             return chain.filter(newExchange);
         };
